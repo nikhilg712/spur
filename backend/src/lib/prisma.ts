840504df474898c76dd4d0581edde58141902324
Nikhil Gupta <nikhil.g@marmeto.com>
2025-12-22 15:56:28 +0530

    prisma configuration

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client"
 import { PrismaLibSql } from "@prisma/adapter-libsql"
-import { createClient } from "@libsql/client"
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient
@@ -18,8 +17,5 @@ export const prisma =
     log: ["query", "info", "warn", "error"],
   })
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
-}
 
 export default prisma
